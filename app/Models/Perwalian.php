b46e3b3d67ed3e3667e74f38adf0719303a26c68
rakyankrisna <rakyankrisna@gmail.com>
2024-06-27 15:12:23 +0000

    update model perwalian

--- app/Models/Perwalian.php
+++ app/Models/Perwalian.php
@@ -1,13 +1,52 @@
+<!-- Suggested code may be subject to a license. Learn more: ~LicenseLog:2085142960. -->
 <?php
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\
+Model;
+
 class Perwalian extends Model
 {
+    use HasFactory;
+
     /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = 'ak_perwalian';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'mahasiswa_id',
+        'dosen_id',
+        'periode_akademik_id',
+        // Tambahkan kolom-kolom lain yang relevan di sini
+    ];
+
+    // Relasi dengan model Mahasiswa
+    public function mahasiswa()
+    {
+        return $this->belongsTo(Mahasiswa::class, 'mahasiswa_id');
+    }
+
+    // Relasi dengan model Dosen
+    public function dosen()
+    {
+        return $this->belongsTo(Dosen::class, 'dosen_id');
+    }
+
+    // Relasi dengan model PeriodeAkademik
+    public function periodeAkademik()
+    {
+        return $this->belongsTo(PeriodeAkademik::class, 'periode_akademik_id');
+    }
+
+    //TODO : Tambahkan scope, fungsi, atau relasi lain sesuai kebutuhan
 }

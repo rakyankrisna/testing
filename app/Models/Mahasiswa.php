4c33a52d7bba346df7b64f8757ffa8372d5d84ef
rakyankrisna <rakyankrisna@gmail.com>
2024-06-27 15:10:03 +0000

    update model Mhs

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,12 +2,46 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
 class Mahasiswa extends Model
 {
+    use HasFactory;
+
     /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = 'ms_mahasiswa';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nim',
+        'nama',
+        'email',
+        'jurusan',
+        'angkatan',
+    ];
+
+    /**
+     * Get the KRS records associated with the mahasiswa.
+     */
+    public function krs()
+    {
+        return $this->hasMany(KRS::class, 'mahasiswa_id');
+    }
+
+    /**
+     * Get the kelas (classes) that the mahasiswa belongs to.
+     */
+    public function kelas()
+    {
+        return $this->belongsToMany(Kelas::class, 'ak_kelas_mahasiswa', 'mahasiswa_id', 'kelas_id');
+    }
 }

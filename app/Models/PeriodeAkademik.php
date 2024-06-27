ad54624bb5a5eaa2febdf37dd9a0d76b06844bac
rakyankrisna <rakyankrisna@gmail.com>
2024-06-27 15:10:20 +0000

    update model periode akademik

--- app/Models/PeriodeAkademik.php
+++ app/Models/PeriodeAkademik.php
@@ -1,13 +1,36 @@
+
+php
 <?php
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\
+Model;
+
 class PeriodeAkademik extends Model
 {
+    use HasFactory;
+
     /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = 'ms_periode_akademik';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        // Daftar kolom yang dapat diisi secara massal, contoh:
+        'nama',
+        'tanggal_mulai',
+        'tanggal_selesai',
+        // ...
+    ];
+
+    //TODO : Tambah relasi, scope, atau fungsi lainnya sesuai kebutuhan
 }

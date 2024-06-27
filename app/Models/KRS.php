5865f3783172cab4fe9e63d7f7fd07ab27f33b3c
rakyankrisna <rakyankrisna@gmail.com>
2024-06-27 15:09:53 +0000

    update model Kelas

--- app/Models/KRS.php
+++ app/Models/KRS.php
@@ -2,12 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
 class KRS extends Model
 {
+    use HasFactory;
+
     /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = 'ak_krs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'mahasiswa_id',
+        'mata_kuliah_id',
+        'semester',
+        'tahun_ajaran',
+    ];
+
+    /**
+     * Get the mahasiswa that owns the KRS.
+     */
+    public function mahasiswa()
+    {
+        return $this->belongsTo(Mahasiswa::class, 'mahasiswa_id');
+    }
+
+    /**
+     * Get the mata kuliah that is associated with the KRS.
+     */
+    public function mataKuliah()
+    {
+        return $this->belongsTo(MataKuliah::class, 'mata_kuliah_id');
+    }
 }
